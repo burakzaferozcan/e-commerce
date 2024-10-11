15292f6283426719a2e97b407dd577b1139aab9f
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-11 21:17:23 +0300

    feature: template integrated on all pages and routes created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
-use App\Http\Controllers\PageHomeController;
+use App\Http\Controllers\Frontend\PageController;
+use App\Http\Controllers\Frontend\PageHomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageHomeController::class, 'index'])->name("Home");
+Route::get('/', [PageHomeController::class, 'index'])->name("home");
+Route::get('/about', [PageController::class, 'about'])->name("about");
+Route::get('/contact', [PageController::class, 'contact'])->name("contact");
+Route::get('/products', [PageController::class, 'products'])->name("products");
+Route::get('/products/detail', [PageController::class, 'product_detail'])->name("product_detail");
+Route::get('/cart', [PageController::class, 'cart'])->name("cart");
+
