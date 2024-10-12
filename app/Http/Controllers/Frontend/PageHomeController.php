2b09e812566e60c8bde6d2c34981ed0699a28157
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-12 15:27:31 +0300

    feature: For the first time, the process of displaying data from the database on the screen has been completed

--- app/Http/Controllers/Frontend/PageHomeController.php
+++ app/Http/Controllers/Frontend/PageHomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class PageHomeController extends Controller
 {
     public function index()
     {
-        return view("frontend.pages.index");
+        $slider = Slider::where("status", "1")->first();
+        $title = "Anasayfa";
+        return view("frontend.pages.index", compact("slider", "title"));
     }
 }
