66a5c0112cf486d9736d2b84e12daed983705d63
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-17 18:08:47 +0300

    products listed on the Products page

--- app/Http/Controllers/Frontend/PageController.php
+++ app/Http/Controllers/Frontend/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\About;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -19,7 +20,8 @@ class PageController extends Controller
     }
     public function products()
     {
-        return view("frontend.pages.products");
+        $products = Product::where("status", "1")->get();
+        return view("frontend.pages.products", compact(("products")));
     }
     public function sale_products()
     {
