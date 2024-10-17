bd09e1ba13d805fa819d13037ea5b8d12f7c584a
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-10-17 17:52:42 +0300

    Products migration created.

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::group(["middleware" => "sitesetting"], function () {
     Route::get('/childrens_products', [PageController::class, 'products'])->name("childrens_products");
     Route::get('/sale_products', [PageController::class, 'sale_products'])->name("sale_products");
 
-    Route::get('/products/detail', [PageController::class, 'product_detail'])->name("product_detail");
+    Route::get('/product/detail', [PageController::class, 'product_detail'])->name("product_detail");
     Route::get('/cart', [PageController::class, 'cart'])->name("cart");
 });
 
