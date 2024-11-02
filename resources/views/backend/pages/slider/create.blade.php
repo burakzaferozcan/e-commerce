24fbea1e1ceee81da9313945c1349dfa9ebf9951
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-11-02 19:48:37 +0300

    feature: the design of the slider creation page has been finalised

--- resources/views/backend/pages/slider/create.blade.php
+++ resources/views/backend/pages/slider/create.blade.php
@@ -4,50 +4,40 @@
     <div class="col-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
-                <h4 class="card-title">Basic form elements</h4>
-                <p class="card-description">
-                    Basic form elements
-                </p>
-                <form class="forms-sample">
+                <h4 class="card-title">Slider Ekle</h4>
+                <form action="{{route('panel.slider.store')}}" class="forms-sample" method="POST" enctype="multipart/form-data">
+                    @csrf
                     <div class="form-group">
-                        <label for="exampleInputName1">Name</label>
-                        <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+                        <label for="name">Başlık</label>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="Slider Başlık">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail3">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
+                        <label for="content">Slogan</label>
+                        <textarea name="content" type="text" class="form-control" id="content" placeholder="Slogan"> </textarea>
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword4">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
+                        <label for="link">Link</label>
+                        <input name="link" type="text" class="form-control" id="link" placeholder="Link">
                     </div>
                     <div class="form-group">
-                        <label for="exampleSelectGender">Gender</label>
-                        <select class="form-control" id="exampleSelectGender">
-                            <option>Male</option>
-                            <option>Female</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>File upload</label>
-                        <input type="file" name="img[]" class="file-upload-default">
+                        <label>Dosya Yükle</label>
+                        <input type="file" name="image" class="file-upload-default">
                         <div class="input-group col-xs-12">
-                            <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+                            <input type="text" class="form-control file-upload-info" disabled placeholder="Fotoğraf Seç">
                             <span class="input-group-append">
-                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+                          <button class="file-upload-browse btn btn-primary" type="button">Seç</button>
                         </span>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputCity1">City</label>
-                        <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
+                        <label for="status">Durum</label>
+                        <select name="status" id="status" class="form-control">
+                            <option value="0">Pasif</option>
+                            <option value="1">Aktif</option>
+                        </select>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleTextarea1">Textarea</label>
-                        <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                    <button class="btn btn-light">Cancel</button>
+                    <button type="submit" class="btn btn-primary mr-2">Gönder</button>
+                    <button class="btn btn-light">İptal</button>
                 </form>
             </div>
         </div>
