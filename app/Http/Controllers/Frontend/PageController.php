4dc17c9d675c7483767d4619eb0c1a212d3808f7
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-11-12 13:09:02 +0300

    refactor: product filtering process has been edited

--- app/Http/Controllers/Frontend/PageController.php
+++ app/Http/Controllers/Frontend/PageController.php
@@ -19,112 +19,49 @@ class PageController extends Controller
     {
         return view("frontend.pages.contact");
     }
-    public function urunler(Request $request,$slug=null) {
+    public function products(Request $request, $slug = null)
+    {
+        $category = request()->segment(1) ?? "";
+        $sizes =!empty($request->size)? explode(",",$request->size) : null;
+        $colors =!empty($request->color)? explode(",",$request->color) : null;
+        $startPrice = $request->min ?? null;
+        $endPrice = $request->max ?? null;
+        $order = $request->order ?? "id";
+        $sort = $request->sort ?? "desc";
 
 
-        $category = request()->segment(1) ?? null;
 
-        $sizes = !empty($request->size) ? explode(',',$request->size) : null;
-
-        $colors = !empty($request->color) ? explode(',',$request->color) : null;
-
-        $startprice = $request->min ?? null;
-
-        $endprice = $request->max ?? null;
-
-        $order = $request->order ?? 'id';
-        $sort = $request->sort ?? 'desc';
-
-
-        $anakategori = null;
-        $altkategori = null;
-        if(!empty($category) && empty($slug)) {
-            $anakategori = Category::where('slug',$category)->first();
-            $categorySlug = $anakategori->slug ?? '';
-        }else if (!empty($category) && !empty($slug)){
-            $anakategori = Category::where('slug',$category)->first();
-            $altkategori = Category::where('slug',$slug)->first();
-            $categorySlug = $altkategori->slug ?? '';
-        }
-
-
-        $breadcrumb = [
-            'sayfalar' => [
-
-            ],
-            'active'=> 'Ürünler'
-        ];
-
-        if(!empty($anakategori) && empty($altkategori)) {
-            $breadcrumb['active'] = $anakategori->name;
-        }
-
-        if(!empty($altkategori)) {
-            $breadcrumb['sayfalar'][] = [
-                'link'=> route($anakategori->slug.'urunler'),
-                'name' => $anakategori->name
-            ];
-
-            $breadcrumb['active'] = $altkategori->name;
-        }
-
-
-        $products = Product::where('status','1')->select(['id','name','slug','size','color','price','category_id','image'])
-            ->where(function($q) use($sizes,$colors,$startprice,$endprice) {
-                if(!empty($sizes)) {
-                    $q->whereIn('size', $sizes);
-                }
-                if(!empty($colors)) {
-                    $q->whereIn('color', $colors);
-                }
-
-                if(!empty($startprice) && $endprice) {
-                    //$q->whereBetween('price', [$startprice,$endprice]);
-
-                    $q->where('price','>=', $startprice);
-
-                    $q->where('price','<=', $endprice);
+        $products = Product::where("status", "1")
+            ->select(["id", "name", "slug", "size", "color", "price", "category_id", "image"])
+            ->where(function ($q) use ($sizes, $colors, $startPrice, $endPrice) {
+                if (!empty($sizes)) {
+                    $q->whereIn("size", $sizes);
+                }if (!empty($colors)) {
+                    $q->whereIn("color", $colors);
+                }if (!empty($startPrice) && !empty($endPrice)) {
+                    $q->whereBetween("price", [$startPrice, $endPrice]);
                 }
                 return $q;
+
             })
-            ->with('category:id,name,slug')
-            ->whereHas('category', function($q) use ($categorySlug) {
-                if(!empty($categorySlug)) {
-                    $q->where('slug', $categorySlug);
+            ->with("category:id,name,slug")
+            ->whereHas("category", function ($q) use ($category, $slug) {
+                if (!empty($slug)) {
+                    $q->where("slug", $slug);
                 }
                 return $q;
-            })
-            ->with('images')
-            ->orderBy($order,$sort)->paginate(21);
+            });
 
-        if($request->ajax()) {
-
-            $view = view('frontend.ajax.productList',compact('products'))->render();
-            return response(['data'=>$view,  'paginate'=>(string) $products->withQueryString()->links('vendor.pagination.custom')]);
-        }
-
-        $sizelists =  Product::where('status','1')->groupBy('size')->pluck('size')->toArray();
-
-        $colors =  Product::where('status','1')->groupBy('color')->pluck('color')->toArray();
-
-        $maxprice = Product::max('price');
+        $sizeList = Product::where("status", "1")->groupBy("size")->pluck("size")->toArray();
+        $colors = Product::where("status", "1")->groupBy("color")->pluck("color")->toArray();
 
 
 
-        $seolists = metaolustur($category);
+        $products = $products->orderBy($order, $sort)->paginate(21);
 
-        $seo = [
-            'title' =>  $seolists['title'] ?? '',
-            'description' => $seolists['description'] ?? '',
-            'keywords' => $seolists['keywords'] ?? '',
-            'image' => asset('img/page-bg.jpg'),
-            'url'=>  $seolists['currenturl'],
-            'canonical'=> $seolists['trpage'],
-            'robots' => 'index, follow',
-        ];
+        $maxprice=Product::max("price");
 
-
-        return view('frontend.pages.products',compact('seo','breadcrumb','products','maxprice','sizelists','colors'));
+        return view("frontend.pages.products", compact((["products", "maxprice", "sizeList", "colors"])));
     }
     public function sale_products()
     {
--- resources/views/frontend/pages/products.blade.php
+++ resources/views/frontend/pages/products.blade.php
@@ -1,8 +1,14 @@
 @extends('frontend.layout.layout')
 
 @section('content')
-
-    @include('frontend.inc.breadcrumb')
+    <div class="bg-light py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mb-0"><a href="index.html">Home</a> <span class="mx-2 mb-0">/</span> <strong
+                        class="text-black">Shop</strong></div>
+            </div>
+        </div>
+    </div>
 
     <div class="site-section">
         <div class="container">
@@ -12,43 +18,76 @@
 
                     <div class="row">
                         <div class="col-md-12 mb-5">
-                            <div class="float-md-left mb-4"><h2 class="text-black h5">Ürünler</h2></div>
+                            <div class="float-md-left mb-4">
+                                <h2 class="text-black h5">Ürünler</h2>
+                            </div>
                             <div class="d-flex">
                                 <div class="dropdown mr-1 ml-md-auto">
 
                                 </div>
                                 <div class="btn-group">
-
-                                    <select class="form-control" id="orderList">
-                                        <option class="dropdown-item" value="">Sıralama Seçiniz</option>
-                                        <option class="dropdown-item" value="id-asc">A-Z ye Sırala</option>
-                                        <option class="dropdown-item" value="id-desc">Z-A ye Sırala</option>
-                                        <option class="dropdown-item" value="price-asc">Düşük Fiyata göre sırala</option>
-                                        <option class="dropdown-item" value="price-desc">Yüksek Fiyata göre sırala</option>
-                                    </select>
+                                    <button type="button" class="btn btn-secondary btn-sm dropdown-toggle"
+                                        id="dropdownMenuReference" data-toggle="dropdown">Sırala</button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuReference">
+                                        <a class="dropdown-item" href="#" data-sira="a_z_order">A-Z'ye Sırala</a>
+                                        <a class="dropdown-item" href="#" data-sira="z_a_order">Z-A'ye Sırala</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#" data-sira="price_min_order">Düşük Fiyata
+                                            Göre</a>
+                                        <a class="dropdown-item" href="#" data-sira="price_max_order">Yüksek Fiyata
+                                            Göre</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="row mb-5 productContent">
-                        @include('frontend.ajax.productList')
+                    <div class="row">
+                        <div class="col-lg-12">
+                            @if(session()->get("success"))
+                                <div class="alert alert-success text-center">{{session()->get("success")}}</div>
+                            @endif
+                        </div>
                     </div>
-                    <div class="row paginateButtons" data-aos="fade-up">
-                        {{$products->withQueryString()->links('vendor.pagination.custom')}}
-                        {{--  <div class="col-md-12 text-center">
-                            <div class="site-block-27">
-                              <ul>
-                                <li><a href="#">&lt;</a></li>
-                                <li class="active"><span>1</span></li>
-                                <li><a href="#">2</a></li>
-                                <li><a href="#">3</a></li>
-                                <li><a href="#">4</a></li>
-                                <li><a href="#">5</a></li>
-                                <li><a href="#">&gt;</a></li>
-                              </ul>
-                            </div>
-                          </div> --}}
+                    <div class="row mb-5">
+
+                        @if (!empty($products) && $products->count() > 0)
+                            @foreach ($products as $product)
+                                <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
+                                    <div class="block-4 text-center border">
+                                        <figure class="block-4-image">
+                                            <a href="{{ route('product_detail', $product->slug) }}"><img
+                                                    src="{{ asset($product->image) }}" alt="Image placeholder"
+                                                    class="img-fluid"></a>
+                                        </figure>
+                                        <div class="block-4-text p-4">
+                                            <h3><a
+                                                    href="{{ route('product_detail', $product->slug) }}">{{ $product->name }}</a>
+                                            </h3>
+                                            <p class="mb-0">{{ $product->short_text }}</p>
+                                            <p class="text-primary font-weight-bold">{{ number_format($product->price, 0) }}
+                                            </p>
+                                            <form action="{{route("cart.add")}}" method="POST">
+                                                @csrf
+                                                <input type="hidden" name="product_id" value="{{$product->id}}">
+                                                <input type="hidden" name="size" value="{{$product->size}}">
+                                               <button type="submit" class="buy-now btn btn-sm btn-primary">Sepete Ekle</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endif
+
+
+
+
+
+
+                    </div>
+                    <div class="row" data-aos="fade-up">
+                        <div class="col-md-12 text-center">
+                            {{ $products->withQueryString()->links('pagination::bootstrap-4') }}
+                        </div>
                     </div>
                 </div>
 
@@ -57,8 +96,10 @@
                         <h3 class="mb-3 h6 text-uppercase text-black d-block">Kategori</h3>
                         <ul class="list-unstyled mb-0">
                             @if (!empty($categories) && $categories->count() > 0)
-                                @foreach ($categories->where('cat_ust',null)  as $category)
-                                    <li class="mb-1"><a href="{{route($category->slug.'urunler')}}" class="d-flex"><span>{{$category->name}}</span> <span class="text-black ml-auto">({{$category->getTotalProductCount()}})</span></a></li>
+                                @foreach ($categories->where('cat_ust', null) as $category)
+                                    <li class="mb-1"><a href="{{ route($category->slug . '_products') }}"
+                                            class="d-flex"><span>{{ $category->name }}</span>
+                                            <span class="text-black ml-auto">({{ $category->getTotalProductCount() }})</span></a></li>
                                 @endforeach
                             @endif
                         </ul>
@@ -68,19 +109,18 @@
                         <div class="mb-4">
                             <h3 class="mb-3 h6 text-uppercase text-black d-block">Fiyata Göre Sırala</h3>
                             <div id="slider-range" class="border-primary"></div>
-                            <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white" disabled="" />
-
-
-                            <input type="text" name="text" id="priceBetween" class="form-control" hidden />
-
+                            <input type="text" name="text" id="amount" class="form-control border-0 pl-0 bg-white"
+                                disabled="" />
                         </div>
 
                         <div class="mb-4">
-                            <h3 class="mb-3 h6 text-uppercase text-black d-block">Boyut</h3>
-                            @if (!empty($sizelists))
-                                @foreach ($sizelists as $key => $size)
+                            <h3 class="mb-3 h6 text-uppercase text-black d-block">Beden</h3>
+                            @if (!empty($sizeList))
+                                @foreach ($sizeList as $key => $size)
                                     <label for="size{{$key}}" class="d-flex">
-                                        <input type="checkbox" value="{{$size}}" id="size{{$key}}" {{isset(request()->size) && in_array($size,explode(',',request()->size)) ? 'checked' : '' }} class="mr-2 mt-1 sizeList"> <span class="text-black">{{$size}}</span>
+                                        <input type="checkbox" id="size{{$key}}" value="{{$size}}" {{isset(request()->size)&& in_array($size,explode(",",request()->size)) ? "checked" : ""}} class="mr-2 mt-1 sizeList"> <span
+                                            class="text-black">{{ $size }}
+                                        </span>
                                     </label>
                                 @endforeach
                             @endif
@@ -88,7 +128,6 @@
 
                         <div class="mb-4">
                             <h3 class="mb-3 h6 text-uppercase text-black d-block">Renk</h3>
-
                             @if (!empty($colors))
                                 @foreach ($colors as $key => $color)
                                     <label for="color{{$key}}" class="d-flex">
@@ -96,14 +135,10 @@
                                     </label>
                                 @endforeach
                             @endif
-
                         </div>
-
-
                         <div class="mb-4">
-                            <button class="btn btn-block btn-primary filterBtn">Filtrele</button>
+                            <button class="btn btn-primary btn-block filterBtn">Filtrele</button>
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -118,14 +153,15 @@
                         </div>
                         <div class="row">
                             @if (!empty($categories))
-                                @foreach ($categories->where('cat_ust',null) as $category)
+                                @foreach ($categories->where('cat_ust', null) as $category)
                                     <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
-                                        <a class="block-2-item" href="{{route($category->slug.'urunler')}}">
+                                        <a class="block-2-item" href="{{ route($category->slug . '_products') }}">
                                             <figure class="image">
-                                                <img src="{{asset($category->image)}}" alt="" class="img-fluid">
+                                                <img src="{{ asset($category->image) }}" alt="" class="img-fluid">
                                             </figure>
                                             <div class="text">
-                                                <h3>{{$category->name}}</h3>
+                                                <span class="text-uppercase">Giyim</span>
+                                                <h3>{{ $category->name }}</h3>
                                             </div>
                                         </a>
                                     </div>
@@ -143,20 +179,14 @@
 
 @section('customjs')
     <script>
-        var maxprice = "{{$maxprice}}";
+        var maxprice = "{{ $maxprice }}";
+        var defaultminprice = "{{request()->min ?? 0 }}";
+        var defaultmaxprice = "{{request()->max ?? $maxprice }}";
 
-        var defaultminprice = "{{request()->min ?? 0}}";
-        var defaultmaxprice = "{{request()->max ?? $maxprice}}";
+        var url=new URL(window.location.href);
 
+        $(document).on("click",".filterBtn",function (e){
 
-        var url = new URL(window.location.href);
-
-        $(document).on('click', '.filterBtn', function(e) {
-            filtrele();
-        });
-
-
-        function filtrele() {
             let colorList  = $(".colorList:checked" ).map((_,chk) => chk.value).get()
             let sizeList = $(".sizeList:checked").map((_,chk) => chk.value).get()
             if (colorList.length  > 0) {
@@ -171,70 +201,11 @@
                 url.searchParams.delete('size');
             }
 
-
-            var price = $('#priceBetween').val().split('-');
-            url.searchParams.set("min", price[0])
-
-            url.searchParams.set("max", price[1])
-
-            newUrl = url.href;
+           var newUrl = url.href;
             window.history.pushState({}, '', newUrl);
-            // location.reload();
+            location.reload();
 
-
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                type:"GET",
-                url:newUrl,
-                success: function (response) {
-
-                    $('.productContent').html(response.data);
-                    $('.paginateButtons').html(response.paginate)
-                }
-            });
-
-
-        }
-
-
-        $(document).on('change', '#orderList', function(e) {
-
-
-            var order = $(this).val();
-
-            if(order != '') {
-                orderby = order.split('-');
-                url.searchParams.set("order", orderby[0])
-                url.searchParams.set("sort", orderby[1])
-            }else {
-                url.searchParams.delete('order');
-                url.searchParams.delete('sort');
-            }
-
-            filtrele();
-
-        });
-
-
-        $(document).on('submit', '#addForm', function(e) {
-            e.preventDefault();
-            const formData = $(this).serialize();
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                type:"POST",
-                url:"{{route('sepet.add')}}",
-                data:formData,
-                success: function (response) {
-                    toastr.success(response.message);
-                    $('.count').text(response.sepetCount);
-                }
-            });
-
-        });
+        })
 
     </script>
 @endsection
